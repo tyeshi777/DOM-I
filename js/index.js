9316edc44996712f24bf00f98727a525d8708d26
Yeshi <tyeshi777@yahoo.com>
2019-03-04 15:50:26 -0800

    almost done

--- js/index.js
+++ js/index.js
@@ -65,6 +65,56 @@ let headingImg = document.getElementById("cta-img");
 headingImg.src = "img/header-img.png";
 heading.alt = "Image of a code snippet.";
 
-let main = siteContent["main-content"];
-let feature = document.querySelector("features-4");
-feature.textContent = "Features";
+const main = siteContent["main-content"];
+let featureHeader = document.querySelectorAll("h4")[0];
+let featurePara = document.querySelectorAll("p")[0];
+
+featureHeader.textContent = "Features";
+featurePara.textContent =
+  "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let aboutHeader = document.querySelectorAll("h4")[1];
+let aboutPara = document.querySelectorAll("p")[1];
+
+aboutHeader.textContent = "About";
+aboutPara.textContent =
+  "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let mainImg = document.getElementById("middle-img");
+mainImg.src = "img/mid-page-accent.jpg";
+mainImg.alt = "Image of code snippets across the screen";
+
+let servicesHeader = document.querySelectorAll("h4")[2];
+let servicesPara = document.querySelectorAll("p")[2];
+
+servicesHeader.textContent = "Services";
+servicesPara.textContent =
+  "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let productHeader = document.querySelectorAll("h4")[3];
+let productPara = document.querySelectorAll("p")[3];
+
+productHeader.textContent = "Product";
+productPara.textContent =
+  "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let visionHeader = document.querySelectorAll("h4")[4];
+let visionPara = document.querySelectorAll("p")[4];
+
+visionHeader.textContent = "Vision";
+visionPara.textContent =
+  "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let contacts = siteContent["contact"];
+let contactHeader = document.querySelectorAll("h4")[5];
+contactHeader.textContent = "Contact";
+contactPara = document.querySelectorAll("p")[5];
+contactPara.textContent = "123 Way 456 Street Somewhere, USA";
+contactPara = document.querySelectorAll("p")[6];
+contactPara.textContent = "1 (888) 888-8888";
+contactPara = document.querySelectorAll("p")[7];
+contactPara.textContent = "sales@greatidea.io";
+
+let footer = siteContent["footer"];
+let footerPara = document.querySelectorAll("p")[8];
+footerPara.textContent = "Copyright Great Idea! 2018";
