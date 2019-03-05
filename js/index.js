99eb03d169856155c29aadb22d2bddf7e2d32ac3
Yeshi <tyeshi777@yahoo.com>
2019-03-04 20:28:10 -0800

    re-done

--- js/index.js
+++ js/index.js
@@ -47,74 +47,61 @@ let logo = document.getElementById("logo-img");
 logo.src = siteContent["nav"]["img-src"];
 logo.alt = "Great Idea! Company logo.";
 
-let navItems = siteContent["nav"];
-let navA = document.querySelectorAll("a");
-navA[0].textContent = navItems["nav-item-1"];
-navA[1].textContent = navItems["nav-item-2"];
-navA[2].textContent = navItems["nav-item-3"];
-navA[3].textContent = navItems["nav-item-4"];
-navA[4].textContent = navItems["nav-item-5"];
-navA[5].textContent = navItems["nav-item-6"];
+const navItems = document.querySelectorAll("nav a");
+navItems.forEach((item, i) => {
+  item.textContent = siteContent.nav[`nav-item-${i + 1}`];
+});
 
-let heading = siteContent["cta"];
-let header1 = document.querySelector("h1");
-header1.textContent = "DOM Is Awesome";
-let btn = document.querySelector("button");
-btn.textContent = "Get Started";
-let headingImg = document.getElementById("cta-img");
-headingImg.src = "img/header-img.png";
-heading.alt = "Image of a code snippet.";
+const header = document.getElementsByClassName("cta-text")[0];
+header.getElementsByTagName("h1")[0].innerHTML = siteContent["cta"]["h1"];
+header.getElementsByTagName("button")[0].innerHTML =
+  siteContent["cta"]["button"];
+const headerImg = document.getElementById("cta-img");
+headerImg.src = siteContent["cta"]["img-src"];
 
-const main = siteContent["main-content"];
-let featureHeader = document.querySelectorAll("h4")[0];
-let featurePara = document.querySelectorAll("p")[0];
+const main = document.querySelectorAll(".text-content");
+main[0].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["features-h4"];
+main[0].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["features-content"];
+main[1].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["about-h4"];
+main[1].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["about-content"];
+main[2].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["services-h4"];
+main[2].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["services-content"];
+main[3].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["product-h4"];
+main[3].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["product-content"];
+main[4].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["vision-h4"];
+main[4].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["vision-content"];
 
-featureHeader.textContent = "Features";
-featurePara.textContent =
-  "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-
-let aboutHeader = document.querySelectorAll("h4")[1];
-let aboutPara = document.querySelectorAll("p")[1];
-
-aboutHeader.textContent = "About";
-aboutPara.textContent =
-  "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-
-let mainImg = document.getElementById("middle-img");
+const mainImg = document.getElementById("middle-img");
 mainImg.src = "img/mid-page-accent.jpg";
 mainImg.alt = "Image of code snippets across the screen";
 
-let servicesHeader = document.querySelectorAll("h4")[2];
-let servicesPara = document.querySelectorAll("p")[2];
+const contact = document.getElementsByClassName("contact")[0];
+contact.getElementsByTagName("h4")[0].textContent =
+  siteContent["contact"]["contact-h4"];
+contact.getElementsByTagName("p")[0].textContent =
+  siteContent["contact"]["address"];
+contact.getElementsByTagName("p")[1].textContent =
+  siteContent["contact"]["phone"];
+contact.getElementsByTagName("p")[2].textContent =
+  siteContent["contact"]["email"];
 
-servicesHeader.textContent = "Services";
-servicesPara.textContent =
-  "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+const footer = document.querySelector("footer");
+footer.getElementsByTagName("p")[0].innerHTML =
+  siteContent["footer"]["copyright"];
 
-let productHeader = document.querySelectorAll("h4")[3];
-let productPara = document.querySelectorAll("p")[3];
-
-productHeader.textContent = "Product";
-productPara.textContent =
-  "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-
-let visionHeader = document.querySelectorAll("h4")[4];
-let visionPara = document.querySelectorAll("p")[4];
-
-visionHeader.textContent = "Vision";
-visionPara.textContent =
-  "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-
-let contacts = siteContent["contact"];
-let contactHeader = document.querySelectorAll("h4")[5];
-contactHeader.textContent = "Contact";
-contactPara = document.querySelectorAll("p")[5];
-contactPara.textContent = "123 Way 456 Street Somewhere, USA";
-contactPara = document.querySelectorAll("p")[6];
-contactPara.textContent = "1 (888) 888-8888";
-contactPara = document.querySelectorAll("p")[7];
-contactPara.textContent = "sales@greatidea.io";
-
-let footer = siteContent["footer"];
-let footerPara = document.querySelectorAll("p")[8];
-footerPara.textContent = "Copyright Great Idea! 2018";
+navItems.forEach(item => {
+  item.style.color = "green";
+});
+const anchor = document.createElement("a");
+anchor.textContent = "Yeshie";
+navItems.append(anchor);
